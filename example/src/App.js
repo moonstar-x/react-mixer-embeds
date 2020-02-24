64b80cd6acfb2eba1868aa553da776c609c2e8c8
moonstar-x <christian.lopez99@outlook.com>
2020-02-24 02:54:48 -0500

    Initialized project.

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-mixer-embed'
-
-export default class App extends Component {
-  render () {
+class App extends Component {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        Example
       </div>
-    )
+    );
   }
 }
+
+export default App;
