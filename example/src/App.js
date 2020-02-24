424193812afd1f2530f5708b459bd54833300b51
moonstar-x <christian.lopez99@outlook.com>
2020-02-24 04:19:07 -0500

    Created MixerPlayer.

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { MixerChat } from 'react-mixer-embed';
+import { MixerChat, MixerPlayer } from 'react-mixer-embed';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MixerChat channel="travpiper" composer={false} />
+        <MixerChat channel="SauceLegendary" />
+        <MixerPlayer channel="SauceLegendary" video="cYihkEpL4kiNLbN4tQrvUQ" time="1h2m10s" allowFullscreen={false} />
       </div>
     );
   }
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,18 @@
-import { MIXER_CHAT_URL } from './constants';
+import { MIXER_CHAT_URL, MIXER_PLAYER_URL } from './constants';
 
 const getChatEmbedURL = (channel, composer) => {
   return `${MIXER_CHAT_URL}/${channel}?composer=${composer}`;
 };
 
+const getPlayerEmbedURL = (channel, { disableCostream, disableLowLatency, muted, disableLinks, hideChannel, vod, t }) => {
+  const vodQuery = vod ? `&vod=${vod}` : '';
+  const tQuery = t ? `&t=${t}` : '';
+  const mutedQuery = muted ? `&muted=${muted}` : '';
+  return `${MIXER_PLAYER_URL}/${channel}?disableCostream=${disableCostream}&disableLowLatency=${disableLowLatency}&disableLinks=${disableLinks}&hideChannel=${hideChannel}${mutedQuery}${vodQuery}${tQuery}`;
+};
+
 export {
-  getChatEmbedURL
+  getChatEmbedURL,
+  getPlayerEmbedURL
+
 };
